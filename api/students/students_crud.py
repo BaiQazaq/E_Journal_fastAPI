5e3c1b08932cb79aced69dbdc6bd8cd42f751952
Isa Bahyt <assib79@gmail.com>
2024-05-31 01:44:27 +0500

    Создана функция чтения и удаления студента: Read (get), Delete

--- api/students/students_crud.py
+++ api/students/students_crud.py
@@ -9,3 +9,15 @@ def create_student(db: Session, student: StudentCreate):
     db.commit()
     db.refresh(db_student)
     return db_student
+
+def get_student(db: Session, student_id: int):
+    return db.query(models.Student).filter(models.Student.id == student_id).first()
+
+def delete_student(db: Session, student_id: int):
+    db_student = get_student(db, student_id)
+    if db_student:
+        db.delete(db_student)
+        db.commit()
+    return db_student
+
+
--- api/students/students_view.py
+++ api/students/students_view.py
@@ -4,10 +4,27 @@ from sqlalchemy.orm import Session
 
 from core.models.database import db_helper
 from .students_schemas import Student, StudentCreate
-from .students_crud import create_student
+from .students_crud import (
+                            create_student as make_student,
+                            get_student, 
+                            delete_student as del_student)
 
 router = APIRouter(tags=["Students"])
 
 @router.post("/", response_model=Student)
-def make_student(student: StudentCreate, db: Session = Depends(db_helper.get_db)):
-    return create_student(db=db, student=student)
+def create_student(student: StudentCreate, db: Session = Depends(db_helper.get_db)):
+    return make_student(db=db, student=student)
+
+@router.get("/{student_id}", response_model=Student)
+def read_student(student_id: int, db: Session = Depends(db_helper.get_db)):
+    db_student = get_student(db=db, student_id=student_id)
+    if db_student is None:
+        raise HTTPException(status_code=404, detail=f"Student by id -> {student_id} not found for showig")
+    return db_student
+
+@router.delete("/{student_id}", response_model=Student)
+def delete_student(student_id: int, db: Session = Depends(db_helper.get_db)):
+    db_student = del_student(db=db, student_id=student_id)
+    if db_student is None:
+        raise HTTPException(status_code=404, detail=f"Student by id -> {student_id} not found for deleting")
+    return db_student
