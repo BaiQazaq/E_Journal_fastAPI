17748ff674f0ce87398ae7510a9cc65917443090
Isa Bahyt <assib79@gmail.com>
2024-05-31 02:26:16 +0500

    Создана функция изменения и удаления балов: Update & Delete. CRUD для Score завершен

--- api/scores/score_crud.py
+++ api/scores/score_crud.py
@@ -14,3 +14,18 @@ def create_score(db: Session, score: ScoreCreate, student_id: int):
 def get_score(db: Session, score_id: int, student_id: int):
     return db.query(models.Score).filter(models.Score.id == score_id, models.Score.student_id == student_id).first()
 
+def update_score(db: Session, score_id: int, score: ScoreUpdate):
+    db_score = db.query(models.Score).filter(models.Score.id == score_id).first()
+    if db_score:
+        for key, value in score.model_dump(exclude_unset=True).items():
+            setattr(db_score, key, value)
+        db.commit()
+        db.refresh(db_score)
+    return db_score
+
+def delete_score(db: Session, score_id: int):
+    db_score = db.query(models.Score).filter(models.Score.id == score_id).first()
+    if db_score:
+        db.delete(db_score)
+        db.commit()
+    return db_score
